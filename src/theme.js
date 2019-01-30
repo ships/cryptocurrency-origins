5909e73c7b6fc750670702089b847e64fe710089
early evening <eve@haut-kultur.org>
2019-01-29 19:22:04 -0800

    adds much substance to talk structure

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import theme from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  font: 'Futura, sans-serif',
+  h1: {
+    textTransform: 'uppercase',
+    fontWeight: 600
+  },
+  monospace: '"Dank Mono", monospace',
+  img: {
+    maxWidth: '100%'
+  },
+  weights: 400,
+  li: {
+    paddingBottom: '20px',
+    fontFamily: '"Dank Mono", monospace'
+  },
+  ul: {
+    listStyle: 'none',
+    paddingLeft: '20px',
+    display: 'inline-block'
+  },
+  colors: {
+    text: '#6AD798',
+    background: 'rgb(1, 22, 39)',
+    link: '#fff',
+    pre: '#fff',
+    preBackground: '#051626',
+    code: '#fff'
+  }
+}
